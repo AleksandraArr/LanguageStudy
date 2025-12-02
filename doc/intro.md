bc843cf9c4179401ee5a84cace00a99b14b6349e
Aleksandra Rakovic <aleksandra.rakovic21@gmail.com>
2025-12-02 20:33:57 +0100

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to language-study
+
+TODO: write [great documentation](https://jacobian.org/writing/what-to-write/)
---
+++ src/language_study/core.clj
@@ -0,0 +1,7 @@
+(ns language-study.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
